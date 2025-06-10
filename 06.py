a171e67df65e8c8ee76e3f3f0ecf69c6139f1084
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-09 22:01:12 -0300

    Inserindo 6 solução

---
+++ 06.py
@@ -0,0 +1,13 @@
+# 6 - Exiba em tela o valor e o tipo de dado da variável num1: Sendo num1 = 1987.
+
+def mostrar_valor(valor):
+    print(valor)
+    print(type(valor))
+
+def atribuir_valor():
+    num1 = 1987
+    return num1
+
+
+
+mostrar_valor(atribuir_valor())
