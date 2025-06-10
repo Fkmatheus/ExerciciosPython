069bd47a0927a347ea06347c023f075d8c2facfa
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-09 22:16:19 -0300

    Inserindo 10 solução

---
+++ 10.py
@@ -0,0 +1,8 @@
+# 10 - Mostre o tamanho da lista nomes / o número de elementos da lista nomes:
+
+nomes = ["Matheus", "Leon", "Sthefany"]
+
+def mostrar_tamanho_lista(lista):
+    print(len(lista))
+
+mostrar_tamanho_lista(nomes)
