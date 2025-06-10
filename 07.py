186ff81d08a849ab5469b77d0c4cc98c315e0322
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-09 22:03:38 -0300

    Inserindo 7 solução.

---
+++ 07.py
@@ -0,0 +1,7 @@
+# 7 - Peça para que o usuário digite um número, em seguida exiba em tela o número digitado.
+
+def pedir_numero():
+    numero = input("Digite um número: ")
+    return numero
+
+print(pedir_numero())
