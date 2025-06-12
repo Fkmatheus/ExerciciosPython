ec83f8df36edf5987bcc6bce7cd503574ff01c9f
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-11 22:38:26 -0300

    Inserindo 13 solução

---
+++ 13.py
@@ -0,0 +1,11 @@
+# Subtraia os valores de num1 e num2:
+
+def subtrai(n1, n2):
+    return n1 - n2
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+subtracao = subtrai(n1, n2)
+
+print(f"Resultado: {subtracao}")
