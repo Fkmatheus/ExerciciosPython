6893badf1d1599018c331b72823b03c1d8da9de8
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-11 22:31:24 -0300

    Inserindo 11 solução

---
+++ 11.py
@@ -0,0 +1,10 @@
+# Some os valores das variáveis num1 e num2: Sendo num1 = 52 e num2 = 106. Por fim exiba em tela o resultado da soma.
+
+def soma(n1, n2):
+    return n1 + n2
+
+
+num1 = 52
+num2 = 106
+
+print(soma(num1, num2))
