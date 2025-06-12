051873bedf9ff7b6ab51d729ae63797bce572b30
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-11 22:58:03 -0300

    Inserindo solução 18

---
+++ 18.py
@@ -0,0 +1,9 @@
+# Verifique se os valores de num1 e de num2 são iguais:
+
+def verifica_igual(n1,n2):
+    return n1 == n2
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite um número: "))
+
+print(verifica_igual(n1,n2))
