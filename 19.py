ecccc282ddea217b0ccd31eec3589a4b80d43aec
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-11 22:59:47 -0300

    Inserindo 19 solução

---
+++ 19.py
@@ -0,0 +1,9 @@
+# Verifique se os valores de num1 e de num2 são diferentes:
+
+def verifica_diferente(n1,n2):
+    return n1 != n2
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite um número: "))
+
+print(verifica_diferente(n1,n2))
