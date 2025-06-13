fb724bf4804d1bccf3bd8fd7b7cebe14732b8171
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-12 21:46:42 -0300

    Inserindo 27 solução

---
+++ 27.py
@@ -0,0 +1,7 @@
+# Crie uma estrutura de repetição que percorre a string ‘Nikola Tesla’, 
+# exibindo em tela letra por letra desse nome:
+
+palavra = "Nikola Tesla"
+
+for letra in palavra:
+    print(letra)
