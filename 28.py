0313371290ed51167a95b006fe4899dfd630c730
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-12 21:48:58 -0300

    Inserindo 28 solução

---
+++ 28.py
@@ -0,0 +1,7 @@
+# Crie uma lista com 8 elementos de uma lista de compras de supermercado, 
+# por meio de um laço de repetição for liste individualmente cada um dos itens dessa lista.
+
+produtos = ["Banana", "Maça", "Melancia", "Coca-cola", "lápis", "caderno", "couve"]
+
+for item in produtos:
+    print(item)
