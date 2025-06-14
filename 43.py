f2ae1bed244babe259b9fc4896d904a1a21dfa47
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-13 22:04:18 -0300

    Inserindo 43 solução

---
+++ 43.py
@@ -0,0 +1,8 @@
+# Mostre o segundo, terceiro e quarto elemento da lista nomes. 
+# Separadamente, mostre apenas o último elemento da lista nomes:
+
+nomes = ['Ana', 'Carlos', 'Daiane', 'Fernando', 'Maria']
+
+print(nomes[1], nomes[2], nomes[3])
+print()
+print(nomes[-1])
