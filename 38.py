fe1269aa03ec563b0ff2917350b815d3acaca7ac
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-13 21:44:02 -0300

    Inserindo 38 solução

---
+++ 38.py
@@ -0,0 +1,6 @@
+# Peça para que o usuário digite um número, 
+# diretamente dentro da função print( ) eleve esse número ao quadrado, exibindo o resultado incorporado a uma mensagem:
+
+n = int(input("Digite um número: "))
+
+print(f"O número {n} ao quadrado é: {n**2}")
