519fa1865929557bd0dc8614df21452adf16a837
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-13 22:09:33 -0300

    Inserindo 44 solução

---
+++ 44.py
@@ -0,0 +1,7 @@
+# Crie um dicionário via método construtor dict( ), 
+# atribuindo para o mesmo ao menos 5 conjuntos 
+# de chaves e valores representando objetos e seus respectivos preços:
+
+dicionario = {'Caneta': 9.32, 'Escova': 12.99, 'Pente': 10.99, 'Caderno': '49.99', 'Borracha': 8.99}
+
+print(dicionario)
