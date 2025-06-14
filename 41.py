f08d48787a2fbf8253da1b590de11df64492416d
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-13 21:50:34 -0300

    Inserindo 41 solução

---
+++ 41.py
@@ -0,0 +1,7 @@
+# Adicione o elemento ‘Eliana’ na lista nomes, especificamente na terceira posição da lista:
+
+nomes = ['Ana', 'Carlos', 'Daiane', 'Fernando', 'Maria']
+
+print(nomes)
+nomes.insert(2, 'Eliana')
+print(nomes)
