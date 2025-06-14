3a579f1164ab125599065c22fa492a2034ff47e2
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-13 22:11:50 -0300

    Inserindo 45 solução

---
+++ 45.py
@@ -0,0 +1,9 @@
+# A partir da seguinte lista [‘C’, ‘JavaScript’, ‘Lua’, ‘Python’] verifique primeiramente e retorne ao usuário se a 
+# linguagem de programação Python consta na lista. Retorne uma mensagem amigável ao usuário para estas duas situações:
+
+lista = ["C", 'JS', 'Lua', 'Python']
+
+if 'Python' in lista:
+    print("Python está na lista")
+else:
+    print("Python não está na lista")
