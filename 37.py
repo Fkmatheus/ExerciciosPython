665216ef313a00bed0c3615f03b8d33d3d30cf39
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-13 21:42:10 -0300

    Inserindo 37 solução

---
+++ 37.py
@@ -0,0 +1,6 @@
+# Declare uma variável que por sua vez recebe um nome digitado pelo usuário, em seguida, exiba em tela uma mensagem de 
+# boas-vindas que incorpora o nome anteriormente digitado, fazendo uso de f’strings.
+
+nome = input("Digite seu nome: ")
+
+print(f"Olá {nome}")
