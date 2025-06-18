7fe2f13122d45aa29fd4d24d2df258a1f2c10697
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-17 21:01:02 -0300

    Inserindo 67 solução

---
+++ 67.py
@@ -0,0 +1,14 @@
+# Crie uma classe de nome Carro e lhe dê três atributos: nome, ano e cor.
+
+class Carro:
+    nome = "Fusca"
+    ano = "2025"
+    cor = "Preto"
+
+
+
+Fusca = Carro()
+
+print(Fusca.nome)
+print(Fusca.ano)
+print(Fusca.cor)
