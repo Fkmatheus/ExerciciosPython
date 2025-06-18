fca39938e39c0a688f606a72a16354a9b4738eee
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-06-17 21:35:17 -0300

    Inserindo 74 solução

---
+++ 74.py
@@ -0,0 +1,5 @@
+# Mostre via terminal a string ‘Bem vindo ao mundo da programação!!!’ de trás para frente usando indexação:
+
+palavra = 'Bem vindo ao mundo da programação!!!'
+
+print(palavra[::-1])
